2ad2e4c04a2fa6595e62a5db5a3809af3ea2efc8
Kiencan <kienbestdaxua@gmail.com>
2025-01-12 00:55:31 +0700

    update

--- modules/user/cart_delete.php
+++ modules/user/cart_delete.php
@@ -3,25 +3,6 @@ if (!defined('_CODE')) {
     die('Access denied');
 }
 
-// $filterAll = filter();
-
-// if (!empty($filterAll['cart_id'])) {
-//     $cartId = $filterAll['cart_id'];
-//     $carttDetail = oneRaw("SELECT * FROM cart WHERE cart_id = $cartId");
-//     if (!empty($carttDetail)) {
-//         $deleteOrder = delete('cart', "cart_id = '$cartId'");
-//         if ($deleteOrder) {
-//             echo 'success';
-//         } else {
-//             echo 'error';
-//         }
-//     }
-// } else {
-//     echo 'error';
-// }
-
-// redirect('?module=user&action=cart&id=' . $filterAll["id"]);
-
 $user_id = getUserIdByToken();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $cart_id = $_POST['cart_id'] ?? null;
